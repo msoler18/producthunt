6a42d5ccd52ed2c3a116b97ab59df59157284487
Miguel Soler <msoler735@gmail.com>
2018-03-11 23:53:20 -0500

    Add Create products Funtionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,25 @@ class ProductsController < ApplicationController
   def index
   	@products = Product.all
   end
+
+  def new
+  	@product = Product.new
+  end
+
+  def create	
+  	@product = Product.new(product_params)
+  	if @product.save
+  		redirect_to products_path
+  	else
+  		@errors = @product.errors.full_messages
+  		render :new
+  	end		
+  end
+
+
+
+  private
+  def product_params
+  	params.require(:product).permit(:name, :url, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'products/index'
-
-	get '/products', to: 'products#index'
+	resources :products
 end
