772585272666af93c6ca9f1fb6640be7cbc38ec0
Miguel Soler <msoler735@gmail.com>
2018-03-13 22:53:54 -0500

    Add delete product function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -35,6 +35,12 @@ class ProductsController < ApplicationController
     end    
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path, notice: "Product has been deleted"
+  end
+
 
 
   private
